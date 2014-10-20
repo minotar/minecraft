0df81577a77c92f576d5460fb035d7249921e0e9
Luke Strickland <luke@axxim.net>
2014-10-19 20:47:03 -0500

    Return a skin hash

--- avatars.go
+++ avatars.go
@@ -2,16 +2,19 @@
 package minecraft
 
 import (
+	"bytes"
+	"crypto/md5"
 	"errors"
 	"fmt"
 	"image"
-	_ "image/png"
+	"image/png"
 	"io"
 	"net/http"
 )
 
 type Skin struct {
 	Image image.Image
+	Hash  string
 }
 
 func GetSkin(u User) (Skin, error) {
@@ -38,7 +41,18 @@ func decodeSkin(r io.Reader) (Skin, error) {
 	if err != nil {
 		return Skin{}, err
 	}
+
+	buf := new(bytes.Buffer)
+	encErr := png.Encode(buf, skinImg)
+	if encErr != nil {
+		return Skin{}, encErr
+	}
+	hasher := md5.New()
+	hasher.Write(buf.Bytes())
+	skinHash := fmt.Sprintf("%x", hasher.Sum(nil))
+
 	return Skin{
 		Image: skinImg,
+		Hash:  skinHash,
 	}, err
 }
